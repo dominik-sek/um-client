a409f50fc45c08e0237222c889c3e6bfeafcabe1
Dominik Sęk <d.sek464@gmail.com>
2022-11-18 11:57:21 +0100

    back to gss

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,3 +24,24 @@ const Home: NextPage = ({ data }:InferGetServerSidePropsType<GetServerSideProps>
   }
 
 export default Home;
+
+// @ts-ignore
+export async function getServerSideProps({req}) {
+  // Fetch data from external API
+  const res = await fetch(process.env.NEXT_PUBLIC_API_URL + '/checkauth',
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Cookie': req.headers.cookie
+      },
+      credentials: 'include',
+
+    })
+  const data = await res.json()
+
+  // Pass data to the page via props
+  return {
+    props: { data }
+  }
+}
