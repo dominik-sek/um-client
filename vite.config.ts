c65bd3e4c0f3260c21daa8a40ec54c582721dbe8
Dominik Sęk <d.sek464@gmail.com>
2023-04-06 00:09:05 +0200

    fix config to match api

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			'/api/v1': {
-				target: 'http://localhost:5678',
+				target: 'https://um-server.onrender.com',
 				changeOrigin: true,
 			},
 		},
