e8714bb731fe7ad3d9e2384b725892062865c22d
Dominik Sęk <d.sek464@gmail.com>
2023-04-07 17:02:39 +0200

    change config

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	server: {
-		proxy: {
-			'/api/v1': {
-				target: 'http://localhost:3000',
-				changeOrigin: true,
+export default ({ mode }) => {
+	process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+	return defineConfig({
+		server: {
+			proxy: {
+				'/api/v1': {
+					target: process.env.VITE_API_URL,
+					changeOrigin: true,
+					secure: true,
+				},
 			},
 		},
-	},
-	plugins: [react()],
-});
+		plugins: [react()],
+	});
+}
